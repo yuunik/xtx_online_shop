04e9ee2af4afdbf6476dffdc2e2d57bba07d3bdb
zhangyinzhou <zhangyinzhou@zonghenggroup.com.cn>
2024-03-19 15:44:10 +0800

    feat: 静态资源导入

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import router from '@/router'
 import App from './App.vue'
+
+// 引入初始化样式
+import '@/styles/common.scss'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
